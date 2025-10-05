f894b1ea76d9d469debe6fc5b88eed0f57c1ac40
Hamza Bouzeboudja <hamzabouzeboudja31@gmail.com>
2025-10-05 21:45:57 +0100

    Add Fiat Design Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // import { Routes } from "react-router-dom";
 // import Navbar from "./components/navbar";
 // import Intro from "./components/Intro";
-import Hero from "./components/Hero";
-import StackedCards from "./components/StackedCards";
-import About from "./components/About";
-import Features from "./components/Features";
+// import Hero from "./components/Hero";
+// import StackedCards from "./components/StackedCards";
+// import About from "./components/About";
+// import Features from "./components/Features";
+import VideoLand from "./components/FiatDesign";
 import ScrollTrigger from "gsap/ScrollTrigger";
 // import { useGSAP } from "@gsap/react";
 // import SplitTextMask from "./components/TestMask";
@@ -20,14 +21,10 @@ function App() {
 
   return (
     <>
-      {/* <Intro /> */}
       <section className="outro h-screen relative p-7 flex items-center justify-center">
         <h1 className="text-2xl lg:text-5xl">This is Intro One</h1>
       </section>
-      <Hero />
-      <About />
-      <StackedCards />
-      <Features />
+      <VideoLand />
       <section className="outro h-screen relative p-7 flex items-center justify-center">
         <h1 className="text-2xl lg:text-5xl">This is Outro One</h1>
       </section>
