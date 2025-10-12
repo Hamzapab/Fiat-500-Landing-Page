6fbb41526cc2de09447b2fd693b188ddbd17781a
Hamza Bouzeboudja <hamza@educhain.io>
2025-10-12 18:34:17 +0100

    Change Intro Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
-import { useState } from "react";
 import Intro from "./components/Intro"
+import Navbar from "./components/Navbar";
 import MainApp from "./MainApp";
 import { useLenis } from "./hooks/useLenis";
 import "./index.css";
@@ -8,16 +8,14 @@ import "./index.css";
 function App() {
   useLenis();
 
-    const [showIntro, setShowIntro] = useState(true);
+   
 
 
   return (
     <>
-      {showIntro ? (
-        <Intro onFinish={() => setShowIntro(false)} />
-      ) : (
-        <MainApp />
-      )}
+      <Navbar />
+      <Intro />
+      <MainApp />
     </>
   );
 }
