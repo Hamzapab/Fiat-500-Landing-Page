dc50b50be673bd4a7aca8a88107caaf29e5da9ea
Hamza Bouzeboudja <hamza@educhain.io>
2025-10-12 12:06:09 +0100

    Add a PreHero Section

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -1,28 +1,63 @@
 // import { Routes } from "react-router-dom";
-// import Navbar from "./components/navbar";
+// import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 // import StackedCards from "./components/StackedCards";
-// import About from "./components/About";
-import Features from "./components/Features";
-import VideoLand from "./components/FiatDesign";
+import About from "./components/About";
+// import Features from "./components/Features";
+// import VideoLand from "./components/FiatDesign";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { gsap } from "gsap";
 import { useLenis } from "./hooks/useLenis";
 import "./index.css";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
 
 function MainApp() {
   useLenis();
   gsap.registerPlugin(ScrollTrigger);
 
+  const introC = useRef(null);
+
+  useGSAP(() => {
+
+    gsap.from(".ele",{
+      opacity: 0,
+      y: 40,
+      duration: 0.8,
+      stagger: 0.2,
+      delay: 0.8
+    })
+  },{scope: introC});
 
   return (
     <>
-      <section className="outro h-screen relative p-7 flex items-center justify-center">
-        <h1 className="text-2xl lg:text-5xl">This is Intro One</h1>
+      {/* <Navbar /> */}
+      <section ref={introC} className="w-screen h-screen">
+        <div>
+          <video
+            src="assets/fiatcanvas.mp4"
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="absolute left-0 top-0 size-full object-cover"
+          ></video>
+        </div>
+        <div className="relative w-full h-full flex items-end p-10 md:pb-20">
+          <div className="pb-10">
+            <h1 className="ele text-lg md:text-3xl mb-5">Welcome to the World of FIAT 500</h1>
+            <h1 className="ele text-lg md:text-3xl mb-5">Discover the FIAT 500 Lineup</h1>
+            <button  className="ele bg-amber-50 rounded-4xl px-6 py-2 text-black cursor-pointer mt-5 
+             hover:bg-amber-100 transition-colors duration-300 ">
+              <a href="#about">Discover The Model</a>
+            </button>
+          </div>
+        </div>
       </section>
+      <About />
       <Hero />
-      <Features />
-      <VideoLand />
+      {/* <Features /> */}
+      {/* <VideoLand /> */}
       <section className="outro h-screen relative p-7 flex items-center justify-center">
         <h1 className="text-2xl lg:text-5xl">This is Outro One</h1>
       </section>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,53 +43,57 @@ export default function Hero() {
     { scope: heroContainer }
   );
   return (
-    <section ref={heroContainer} className="intro p-0 m-0">
-      <div className="introParent w-full h-[100vh] flex items-center justify-center">
-        <div className="IntroVid w-[90%] h-[90vh] rounded-md overflow-hidden">
-          <div className="vidLayer absolute inset-0 flex items-center justify-center pb-8">
-            <h1 className="text-white text-3xl md:text-5xl font-bold drop-shadow-lg pt-5 lg:pt-20 
-              ">
-              <SplitTextMask text="FIAT 500" className="" delay={0} />
-              <SplitTextMask
-                text="Where Style Meets Motion."
-                className=""
-                delay={0}
-              />
-            </h1>
+    <>
+      <section ref={heroContainer} className="intro p-0 m-0">
+        <div className="introParent w-full h-[100vh] flex items-center justify-center">
+          <div className="IntroVid w-[90%] h-[90vh] rounded-md overflow-hidden">
+            <div className="vidLayer absolute inset-0 flex items-center justify-center pb-8">
+              <h1
+                className="text-white text-3xl md:text-5xl font-bold drop-shadow-lg pt-5 lg:pt-20 
+              "
+              >
+                <SplitTextMask text="FIAT 500" className="" delay={0} />
+                <SplitTextMask
+                  text="Where Style Meets Motion."
+                  className=""
+                  delay={0}
+                />
+              </h1>
+            </div>
+            <video
+              src="/assets/intro.mp4"
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="w-full h-full object-cover"
+            />
           </div>
-          <video
-            src="/assets/intro.mp4"
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="w-full h-full object-cover"
-          />
         </div>
-      </div>
-      <div className="introTxt mt-4 lg:mt-150 flex items-center justify-between px-8">
-        <div>
-          <h3 className="text-xl">
-            Experience the <br /> spirit of the <br />{" "}
-            <span className="text-amber-300 py-2">FIAT 500</span> <br />
-            where style <br></br> meets innovation.
-          </h3>
+        <div className="introTxt mt-4 lg:mt-150 flex items-center justify-between px-8">
+          <div>
+            <h3 className="text-xl">
+              Experience the <br /> spirit of the <br />{" "}
+              <span className="text-amber-300 py-2">FIAT 500</span> <br />
+              where style <br></br> meets innovation.
+            </h3>
+          </div>
+          <ul>
+            <li className="py-2">
+              <h5 className="text-amber-300 py-2">. Iconic Design</h5>A timeless
+              silhouette with <br></br> a modern twist.
+            </li>
+            <li className="py-2">
+              <h5 className="text-amber-300 py-2">. Italian Spirit</h5>
+              Crafted with passion,<br></br> made to stand out.
+            </li>
+            <li className="py-2">
+              <h5 className="text-amber-300 py-2">. Urban Freedom</h5>
+              Compact, efficient, and <br></br> perfect for the city.
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li className="py-2">
-            <h5 className="text-amber-300 py-2">. Iconic Design</h5>A timeless
-            silhouette with <br></br> a modern twist.
-          </li>
-          <li className="py-2">
-            <h5 className="text-amber-300 py-2">. Italian Spirit</h5>
-            Crafted with passion,<br></br> made to stand out.
-          </li>
-          <li className="py-2">
-            <h5 className="text-amber-300 py-2">. Urban Freedom</h5>
-            Compact, efficient, and <br></br> perfect for the city.
-          </li>
-        </ul>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
