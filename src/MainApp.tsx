afe969af656f8bb8cea02327e32f988655256b41
Hamza Bouzeboudja <hamza@educhain.io>
2025-10-18 16:52:42 +0100

    Add Footer Section

--- src/MainApp.tsx
+++ src/MainApp.tsx
@@ -14,19 +14,45 @@ function MainApp() {
   useLenis();
   gsap.registerPlugin(ScrollTrigger);
 
-
- 
-
   return (
     <>
-      {/* <About /> */}
+      <About />
       <StackedCards />
+      <Features />
       <Hero />
-      {/* <Features /> */}
       <VideoLand />
-      <section className="outro h-screen relative p-7 flex items-center justify-center">
-        <h1 className="text-2xl lg:text-5xl">This is Outro One</h1>
-      </section>
+      <footer className="py-15 pt-40 bg-white text-black  px-8 flex flex-col md:flex-row items-center justify-between">
+        <div className="text-center md:text-left">
+          <h4 className="text-lg font-semibold">FIAT 500 Experience</h4>
+          <p className="text-sm text-black">
+            Crafted with passion and precision — inspired by Italian design.
+          </p>
+        </div>
+
+        <div className="flex gap-6 mt-4 md:mt-0">
+          <a
+            href="https://hamzapab.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-amber-300   transition"
+          >
+            Portfolio
+          </a>
+          <a
+            href="https://github.com/HamzaPab"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-amber-300 transition"
+          >
+            GitHub
+          </a>
+        </div>
+
+        <div className="mt-4 md:mt-0 text-sm text-gray-900 text-center md:text-right">
+          © {new Date().getFullYear()} — Built by{" "}
+          <span className="text-amber-300 font-medium">Hamza Pab</span>
+        </div>
+      </footer>
     </>
   );
 }
